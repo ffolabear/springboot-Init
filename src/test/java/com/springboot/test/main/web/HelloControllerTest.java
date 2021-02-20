5ff4a6d7b4f834a931f67e9301b3c386eebbcead
ffbear <kth6699@naver.com>
2021-02-21 00:10:37 +0900

    Lombok 추가 Lombok 활용한 Dto 패키지 및 테스트코드 추가

--- src/test/java/com/springboot/test/main/web/HelloControllerTest.java
+++ src/test/java/com/springboot/test/main/web/HelloControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -28,13 +29,20 @@ public class HelloControllerTest {
     }
 
     @Test
-    public void helloDto가_리턴된다(){
-
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
-
-
+        // .param으로 사용될 요청 파라미터 설정 (String 만 허용)
+        // jsonPath : JSON 응답값을 필드별로 검증할수있는 메소드
+        // $ 를 기준으로 필드명 명시
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 
