fe3b7b5f192a3fbec2065698ccd5252d0fe37116
ffbear <kth6699@naver.com>
2021-02-20 23:56:49 +0900

    Lombok 추가 Lombok 활용한 Dto 패키지 및 테스트코드 추가

--- src/main/java/com/springboot/test/main/web/HelloController.java
+++ src/main/java/com/springboot/test/main/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot.test.main.web;
 
+import com.springboot.test.main.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // JSON을 반환하는 컨트롤러로 만들어줌
@@ -14,5 +16,19 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    // requestParam : 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+    // @RequestParam("name") 를 String name에 저장
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name, amount);
+    }
+
+
+
+
+
 
 }
--- src/test/java/com/springboot/test/main/web/HelloControllerTest.java
+++ src/test/java/com/springboot/test/main/web/HelloControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -27,4 +26,31 @@ public class HelloControllerTest {
                 andExpect(content().string(hello));
 
     }
+
+    @Test
+    public void helloDto가_리턴된다(){
+
+        String name = "hello";
+        int amount = 1000;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
